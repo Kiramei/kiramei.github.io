772c3f10737dfd8d69f6bfe00531a893ab2f08e2
Kiramei <1710623513@qq.com>
2022-11-30 23:35:12 +0800

    fix

--- app.vue
+++ app.vue
@@ -2,6 +2,6 @@
   <Page />
 </template>
 
-<script setup>
+<script lang="ts" setup>
 import Page from './pages/start.vue'
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    app: {
+        head: {
+            charset: 'utf-16',
+            viewport: 'width=500, initial-scale=1',
+            title: 'Kiramei Official Site',
+            htmlAttrs: {
+                lang: 'jp'
+            },
+            meta: [{
+                charset: 'utf-8'
+            },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1'
+            },
+            {
+                hid: 'description',
+                name: 'description',
+                content: ''
+            },
+            {
+                name: 'format-detection',
+                content: 'telephone=no'
+            }
+            ],
+            link: [{
+                rel: 'icon',
+                type: 'image/x-icon',
+                href: 'https://raw.githubusercontent.com/Kiramei/kiramei.github.io/main/fav.ico'
+            }],
+        }
+    }
 })
