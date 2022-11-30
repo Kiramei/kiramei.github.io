b410add5753d765aea06e81de9231259dfe8f21b
Kiramei <1710623513@qq.com>
2022-12-01 00:04:44 +0800

    ttfix

--- app.vue
+++ app.vue
@@ -4,4 +4,30 @@
 
 <script setup>
 import Page from './pages/start.vue'
+useHead({
+  charset: 'utf-16',
+  viewport: 'width=500, initial-scale=1',
+  title: 'Kiramei Official Site',
+  htmlAttrs: {
+    lang: 'jp'
+  },
+  meta: [{
+    charset: 'utf-8'
+  },
+  {
+    name: 'viewport',
+    content: 'width=device-width, initial-scale=1'
+  },
+  {
+    hid: 'description',
+    name: 'description',
+    content: `This is Kiramei's official site. Activities are posted here`
+  }
+  ],
+  link: [{
+    rel: 'icon',
+    type: 'image/x-icon',
+    href: 'https://raw.githubusercontent.com/Kiramei/kiramei.github.io/main/fav.ico'
+  }]
+})
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,31 +1,3 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    app: {
-        head: {
-            charset: 'utf-16',
-            viewport: 'width=500, initial-scale=1',
-            title: 'Kiramei Official Site',
-            htmlAttrs: {
-                lang: 'jp'
-            },
-            meta: [{
-                charset: 'utf-8'
-            },
-            {
-                name: 'viewport',
-                content: 'width=device-width, initial-scale=1'
-            },
-            {
-                hid: 'description',
-                name: 'description',
-                content: `This is Kiramei's official site. Activities are posted here`
-            }
-            ],
-            link: [{
-                rel: 'icon',
-                type: 'image/x-icon',
-                href: 'https://raw.githubusercontent.com/Kiramei/kiramei.github.io/main/fav.ico'
-            }],
-        }
-    }
 })
